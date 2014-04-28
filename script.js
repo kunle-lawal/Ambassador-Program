58f9294505c8c15af0ff98546b242d966e9efc07
Happy-Loman <olawal@ccscfiles.org>
2014-04-27 22:59:37 -0400

    Made a lot of cahnges, Cahnged the look of the page, and added a bit of jQuery function, just for fun

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//this is a javascript comment
+$(document).ready(function() {
+	alert("hi");
+	function opacity(id, value){
+		return $(id).css("opacity", value);
+	}
+	//random number generator
+function randNum( min, max ) { 
+    return Math.random() * ( max - min ) + min;    
+}
+ 
+	$("#login").click(function(){
+		document.getElementById('ambasador').innerHTML = "Ambasador"
+	})
+
+
+	window.setInterval(function(){
+		var randCol =  randNum(1, 2)
+		if (randCol >= 1 && randCol <= 1.5){
+			$("#ambasador").css('color', 'gray');
+		}
+		if (randCol >= 1.5 && randCol <= 2) {		
+			$("#ambasador").css('color', '#003366');
+		};
+		console.log(randCol);
+	}, 1000)
+
+});
