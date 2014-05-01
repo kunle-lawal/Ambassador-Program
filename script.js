871f38c9d79a926e9f0254005d0a70ea33708b67
Happy-Loman <mrhappyloman@gmail.com>
2014-05-01 00:21:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-$(document).ready(function() {
-	alert("hi");
-	function opacity(id, value){
-		return $(id).css("opacity", value);
-	}
 	//random number generator
 function randNum( min, max ) { 
     return Math.random() * ( max - min ) + min;    
 }
  
-	$("#login").click(function(){
-		document.getElementById('ambasador').innerHTML = "Ambasador"
-	})
+$(document).ready(function() {
+	$("#theprogram").click(function(event) {
+		/* Act on the event */
+		$("#welcome").fadeOut('slow')
+		$("#info").fadeIn("slow", function() {
+			$("#info").css('opacity', 1);
+		});
+	});
 
 
-	window.setInterval(function(){
-		var randCol =  randNum(1, 2)
-		if (randCol >= 1 && randCol <= 1.5){
-			$("#ambasador").css('color', 'gray');
-		}
-		if (randCol >= 1.5 && randCol <= 2) {		
-			$("#ambasador").css('color', '#003366');
-		};
-		console.log(randCol);
-	}, 1000)
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
 
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('#who').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('#who').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+
+    	
+	}
+
+	$("#who").click(function(event) {
+		/* Act on the event */
+		animateDiv();
+	});
 });
