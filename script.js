26a9554b72f8a3a4a58293481f4e308bd8098bfd
Happy-Loman <mrhappyloman@gmail.com>
2014-05-14 23:18:18 -0400

    Updated the script.js to fix the navigation bar.

--- script.js
+++ script.js
@@ -1,34 +1,105 @@
-//random number generator
-function randNum( min, max ) { 
-    return Math.random() * ( max - min ) + min;    
+$(document).ready(function(){
+  
+  var maxLength = 40;
+  
+  var length = 0;
+  
+  function randNum( min, max ) {
+    return Math.random() * ( max - min ) + min;
 }
- 
-$(document).ready(function() {
-    $("#info").hide();
-    $("#socInfo").hide();
-    $("#end").hide();
 
+  function changePages(){
+    $("#home").click(function(event) {
+      $(this).css("box-shadow", "inset 1px 10px 18px 3px rgba(0, 0, 0, 0.89)");
+    });
+  }
 
-    function remove(home, info, socInfo, end){
-        if ()
+  changePages();
+
+  
+  var sepe = window.setInterval(function(){
+    length += 10;
+    $("#seperate").css("width", length +"%");
+    
+    if(length >= 40){
+      window.clearInterval(sepe);
     }
-
-	$("#home").click(function(){
-        $("#welcome").fadeIn("slow"); 
-        $("#home").css("text-shadow", "0 0 10px white");
-        $("#info").fadeOut("fast");
-        $("#socInfo").fadeOut("fast");
-        $("#end").fadeOut("fast");
-    })
-});
-
-
-function fade(In, Out, id, speed){
-if(In === true){
- $(id).fadeIn(speed);
-} else if (Out === true) {
- $(id).fadeOut(speed);
-}
-}
-
-fade(true, false, "#welcome", "fast"); 
+  }, 1000/10);
+  
+  
+    window.setInterval(function(){
+      var randTestimo = randNum(0, 4);
+      
+      function getOut(easy, solution, coolTool, loving){
+        if(easy === true){
+          $("#easy").css({
+            color:"#ffffff",
+            opacity:"0"
+          });
+        }
+        if(solution === true){
+          $("#solution").css({
+            color:"#ffffff",
+            opacity:"0"
+          });
+        }
+        if(coolTool === true){
+          $("#coolTool").css({
+            color:"#ffffff",
+            opacity:"0"
+          });
+        }
+        if(loving === true){
+          $("#loving").css({
+            color:"#ffffff",
+            opacity:"0"
+          });
+        }
+      }
+      
+      
+      if(randTestimo <= 1){
+        $("#easy").css({
+          color:"#5f625a",
+          opacity:"0.7"
+        });
+        getOut(false, true, true, true);
+      } else if(randTestimo <= 2 && randTestimo >= 1.09){
+        $("#solution").css({
+          color:"#5f625a",
+          opacity:"0.7"
+        });
+        getOut(ture, false, true, true);
+      } else if(randTestimo <= 3 && randTestimo >= 2.09){
+        $("#coolTool").css({
+          color:"#5f625a",
+          opacity:"0.7"
+        });
+        getOut(true, true, false, true);
+      } else if(randTestimo <= 4 && randTestimo >= 3.09){
+        $("#loving").css({
+          color:"#5f625a",
+          opacity:"0.7"
+        });
+        getOut(true, true, true, false);
+      }
+      
+    }, randNum(1, 10)*1000);
+    
+  
+ /* $(".proclaim").click(function(){
+   $(".proclaim").css("transform", "perspective(800px) rotate3d(1, 0, 0, 17deg)");
+    $(".proclaim").css("opacity", "0.7");
+ });
+  */
+     var scroll =  $(document).scrollTop();   
+  
+  
+      if(scroll >= 700){
+        alert("Hi");  
+      }
+  
+ 
+});  
+   
+   
